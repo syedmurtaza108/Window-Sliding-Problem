5912ad2a5a78e390df27055316a3daee0219d947
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-10-20 09:35:51 +0500

    longest sub string method added

--- Window Sliding Problem/Program.cs
+++ Window Sliding Problem/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Window_Sliding_Problem
 {
     class Program
     {
+        static Dictionary<char, int> list = new Dictionary<char, int>();
         static void Main(string[] args)
         {
-            Console.WriteLine(MaxSumSubArray(new int[] { 4, 2, 1, 7, 8, 1, 2, 8, 1, 0 }, 3));
-            Console.WriteLine(SmallestSubArrayGivenSum(new int[] { 4, 2, 2, 7, 8, 1, 2, 8, 1, 0 }, 8));
-            Console.WriteLine(SmallestSubArrayGivenSum(new int[] { 1, 1, 1, 1, 1, 1, 1, 1 }, 11));
+            //Console.WriteLine(MaxSumSubArray(new int[] { 4, 2, 1, 7, 8, 1, 2, 8, 1, 0 }, 3));
+            //Console.WriteLine(SmallestSubArrayGivenSum(new int[] { 4, 2, 2, 7, 8, 1, 2, 8, 1, 0 }, 8));
+            //Console.WriteLine(SmallestSubArrayGivenSum(new int[] { 1, 1, 1, 1, 1, 1, 1, 1 }, 11));
+            Console.WriteLine(LongestSubStringGivenDistinctCharacter("aabacbebebe", 3));
+            //AAAHHIBC
         }
 
         static int MaxSumSubArray(int[] array, int k)
@@ -31,10 +35,10 @@ namespace Window_Sliding_Problem
         {
             int currentSum = 0;
             int startIndex = 0, size = int.MaxValue;
-            for (int i = 0; i < array.Length; )
+            for (int i = 0; i < array.Length;)
             {
                 currentSum += array[i];
-                if(currentSum >= targetSum)
+                if (currentSum >= targetSum)
                 {
                     size = Math.Min(i - startIndex + 1, size);
                     currentSum -= array[startIndex];
@@ -45,6 +49,48 @@ namespace Window_Sliding_Problem
                 i++;
             }
             return size == int.MaxValue ? 0 : size;
+        }
+
+        static int LongestSubStringGivenDistinctCharacter(string s, int k)
+        {
+            for (int i = 0; i < s.Length + 1; )
+            {
+                if (list.Count == k + 1)
+                {
+                    if (list.GetValueOrDefault(s[i], 1) == 1) list.RemoveAt(0);
+                    else
+                    {
+                        list.Insert(0, KeyValuePair.Create(s[0], list[0].Value - 1));
+                        list.RemoveAt(1);
+                    }
+                    continue;
+                }
+
+                if(i < s.Length)
+                {
+                    bool added = false;
+                    list.get
+                    for (int j = 0; j < list.Count; j++)
+                    {
+                        if (list[j].Key == s[i])
+                        {
+                            added = true;
+                            list.Insert(j, KeyValuePair.Create(s[i], list[j].Value + 1));
+                            list.RemoveAt(j + 1);
+                        }
+                    }
+
+                    if (!added) list.Add(KeyValuePair.Create(s[i], 1));
+                }
+                i++;
+            }
+
+            int totalLenght = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                totalLenght += list[i].Value;
+            }
+            return totalLenght;
         }
     }
 }
